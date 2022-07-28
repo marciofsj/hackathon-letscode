046b8884c2eb231ce935d3fc1ea3b598bab4e3e4
unknown <victo_phone@hotmail.com>
2022-07-28 15:25:49 -0300

    validacao resolvida

--- src/pages/TeacherAccount/index.jsx
+++ src/pages/TeacherAccount/index.jsx
@@ -6,36 +6,45 @@ import { validEmail, validPassword } from '../../utils/regex.js'
 import logomarca from '../../assets/images/Edutrip.png'
 
 export function NewTeacherAccount() {
-  const [newname, setNewName] = useState('');
-  const [newemail, setNewEmail] = useState('');
-  const [newrg, setNewRG] = useState('');
-  const [newcpf, setNewCPF] = useState('');
-  const [newdate, setNewDate] = useState('');
-  const [newpassword, setNewPassword] = useState('');
-  const [repeatpassword, setRepetNewPassword] = useState('');
-  const [validated, setValitadion] = useState(false)
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newRG, setNewRG] = useState('');
+  const [newCPF, setNewCPF] = useState('');
+  const [newDate, setNewDate] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [repeatPassword, setRepetNewPassword] = useState('');
+  const [validated, setValitadion] = useState(true)
   const navigate = useNavigate()
 
   const [emailErr, setEmailErr] = useState(true);
   const [pwdError, setPwdError] = useState(true);
 
   const validate = () => {
-    if (validEmail.test(newemail)) {
-      return setEmailErr(false);
-    } else {
-      alert('Email inválido')
-      return setEmailErr(true)
-    }
-    if ((validPassword.test(newpassword)) && newpassword === repeatpassword) {
-      return setPwdError(false);
-    } else {
-      alert('Senha inválida');
-      return setPwdError(true)
-    }
 
-    if (!pwdError && !emailErr) {
-      navigate('/professor')
+    if (!validEmail.test(newEmail) || ((!validPassword.test(newPassword)) || newPassword !== repeatPassword)) {
+      
+      if (!validEmail.test(newEmail)) {
+        return alert('Email inválido');
+      }
+      if ((!validPassword.test(newPassword)) 
+        || newPassword !== repeatPassword) {
+        return alert('Senha inválida');
+      }
+      setValitadion(false)
+    } else {
+
+      if (validated) {
+        saveLogin()
+        navigate('/professor');
+      }
     }
+  }
+
+  const saveLogin = () => {
+    localStorage.setItem('teachersAccount', JSON.stringify({
+      'email': newEmail,
+      'password': newPassword
+    }))
   }
 
   return (
@@ -50,7 +59,7 @@ export function NewTeacherAccount() {
                     type="text"
                     name="nome"
                     placeholder="insira seu nome"
-                    value={newname}
+                    value={newName}
                     onChange={e => setNewName(e.target.value)}
                 />*/}
         <p>E-MAIL</p>
@@ -58,7 +67,7 @@ export function NewTeacherAccount() {
           type="email"
           name="e-mail"
           placeholder="insira seu e-mail"
-          value={newemail}
+          value={newEmail}
           onChange={e => setNewEmail(e.target.value)}
         />
         {/*<p>RG</p>
@@ -66,7 +75,7 @@ export function NewTeacherAccount() {
                     type="number"
                     name="RG"
                     placeholder="insira seu RG"
-                    value={newrg}
+                    value={newRG}
                     onChange={e => setNewRG(e.target.value)}
                 />
                 <p>CPF</p>
@@ -74,14 +83,14 @@ export function NewTeacherAccount() {
                     type="number"
                     name="CPF"
                     placeholder="insira seu CPF"
-                    value={newcpf}
+                    value={newCPF}
                     onChange={e => setNewCPF(e.target.value)}
                 />
                 <p>DATA DE NASCIMENTO</p>
                 <input
                     type="date"
                     name="data de nascimento"
-                    value={newdate}
+                    value={newDate}
                     onChange={e => setNewDate(e.target.value)}
                 />*/}
         <p>SENHA</p>
@@ -89,7 +98,7 @@ export function NewTeacherAccount() {
           type="password"
           name="nova senha"
           placeholder="crie uma senha"
-          value={newpassword}
+          value={newPassword}
           onChange={e => setNewPassword(e.target.value)}
         />
         <p>REPITA A SENHA</p>
@@ -97,7 +106,7 @@ export function NewTeacherAccount() {
           type="password"
           name="repetir senha"
           placeholder="repita a sua senha"
-          value={repeatpassword}
+          value={repeatPassword}
           onChange={e => setRepetNewPassword(e.target.value)}
         />
       </InputContainer>
